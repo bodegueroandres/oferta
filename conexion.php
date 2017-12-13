7bb802f717f258f7143f11e7c5fa372eccf7d0d6
bodegueroandres <exequiel_puche@hotmail.com>
2017-12-13 16:53:24 -0300

    Modificacion de estilos

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$mysqli = new mysqli("localhost","root","","oferta"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+	$mysqli = new mysqli("localhost","id3656091_root","belgrano","id3656091_oferta"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 	$acentos = $mysqli->query("SET NAMES 'utf8'");//la verdad de la milanesa
 	
 	if(mysqli_connect_errno()){
