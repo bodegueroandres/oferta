87317dac349db200f19ba9c1e0fadaf3dcd2d3e6
bodegueroandres <exequiel_puche@hotmail.com>
2017-12-14 20:02:20 -0300

    Entrega Final

--- admins.php
+++ admins.php
@@ -6,7 +6,7 @@ require 'conexion.php';
 <head>
 	<meta charset="UTF-8">
 	<title>Administrador</title>
-	<link rel="stylesheet" href="estilos.css">
+	<link rel="stylesheet" href="admins.css">
 </head>
 <body>
 		<div class="contenedor">
@@ -34,23 +34,28 @@ require 'conexion.php';
 									$queryAdmin="SELECT nombre, email,detalle_consulta FROM `datos`";
 									$resultadoAdmin = $mysqli->query($queryAdmin);
 									?>
-							<table border="1" class="tablaResultados">
-									<tr>
-										<td><strong>Nombre</strong></td>
-										<td><strong>Email</strong></td>
-										<td><strong>Detalle de Consulta</strong></td>
+							<div class="tabla">
+								
 
-									</tr>
-									<?php		
-										while($rowA = $resultadoAdmin->fetch_assoc()){
-											echo "<tr>";
-											echo "<td>".$rowA['nombre']."</td>";
-											echo "<td>".$rowA['email']."</td>";
-											echo "<td>".$rowA['detalle_consulta']."</td>";
-											echo "</tr>";
-										}
-							 		?>	
-							</table>
+									<table border="1" class="tablaResultados">
+											<tr>
+												<td><strong>Nombre</strong></td>
+												<td><strong>Email</strong></td>
+												<td><strong>Detalle de Consulta</strong></td>
+
+											</tr>
+											<?php		
+												while($rowA = $resultadoAdmin->fetch_assoc()){
+													echo "<tr>";
+													echo "<td>".$rowA['nombre']."</td>";
+													echo "<td>".$rowA['email']."</td>";
+													echo "<td>".$rowA['detalle_consulta']."</td>";
+													echo "</tr>";
+												}
+									 		?>	
+									</table>
+							</div>
+							
 							</div>
 				</div>
 				<footer >
--- guarda.php
+++ guarda.php
@@ -14,50 +14,89 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Oferta Educativa Belgrano</title>
+	<link rel="stylesheet" href="guarda.css">
+
 </head>
 <body>
-	<a href="index.php">INICIO</a>
-	<?php 
 
-		$id_sede = $_POST['cbx_estado'];
-		$queryInstitucion="
-							
-							SELECT `tipo`,`turno`,`nombre`,`direccion`,`telefono`,`email`,`localizacion`
-							FROM `institucion`
-							WHERE `fk_sede`= '$id_sede'
-							";
-	$resultadoI = $mysqli->query($queryInstitucion);
-	?>
-	<table border="1" class="tablaResultados">
-			
-					<?php
-					$rowM = $resultadoI->fetch_assoc();//
-					foreach ($rowM as $key => $value) {
-						echo "<tr><td>".$key.": <br><hr/>".$value."<br></td></tr>" ;
-					}
-		
-	 ?>	
-	</table>
+	<div class="contenedor">
+				<header>
+					<div class="logo">
+						<img src="A2B4BF12B.png" alt="">
+					</div>
+					<div class="titulo">
+						<h1>9-008 Instituto Manuel Belgrano</h1>
+						<h3>Oferta Educativa</h3>
 
-<?php 
-//Ahora registraremos en la base de datos la Consulta para que pueda ser visualizada por los admins
-if ($_POST['usuario']=="" || $_POST['email']=="" ) {
-	$nombreU='anonimo';
-	$emailU='anonimo';
-}else{
-	
-	$nombreU=$_POST['usuario'];
-	$emailU=$_POST['email'];
-	echo "Usuario :".$nombreU." email: ".$emailU;
-}
+					</div>
+					<div class="tituloresp">
+						<h1>9-008 Instituto Manuel Belgrano</h1>
+						<h3>Oferta Educativa</h3>
+					</div>
+				</header>
 
-//ahora enviamos los datos a la base de datos para que la visualice
+				<div class="main">
 
-$sql = "INSERT INTO datos (id_datos,nombre,email) VALUES(NULL,'$nombreU','$emailU')";
-	$resultado = $mysqli->query($sql);
- ?>
+								<a href="index.php" class="myButton">INICIO</a>
+								<br/>
+								<br/>
+								<h1>RESULTADOS DE CONSULTA</h1>
+								<br/>
+								<?php 
 
-	
+									$id_sede = $_POST['cbx_estado'];
+									$queryInstitucion="
+														
+														SELECT `tipo`,`turno`,`nombre`,`direccion`,`telefono`,`email`,`localizacion`
+														FROM `institucion`
+														WHERE `fk_sede`= '$id_sede'
+														";
+								$resultadoI = $mysqli->query($queryInstitucion);
+								?>
+								<div class="tabla">
+									
+									<table border="1" class="tablaResultados">
+										
+												<?php
+												$rowM = $resultadoI->fetch_assoc();//
+												foreach ($rowM as $key => $value) {
+													echo "<tr><td>".$key.": <br><hr/>".$value."<br></td></tr>" ;
+												}
+									
+												 ?>	
+									</table>
+
+								</div>
+								
+
+							<?php 
+							//Ahora registraremos en la base de datos la Consulta para que pueda ser visualizada por los admins
+							if ($_POST['usuario']=="" || $_POST['email']=="" ) {
+								$nombreU='anonimo';
+								$emailU='anonimo';
+							}else{
+								
+								$nombreU=$_POST['usuario'];
+								$emailU=$_POST['email'];
+								echo "Usuario :".$nombreU." email: ".$emailU;
+							}
+
+							//ahora enviamos los datos a la base de datos para que la visualice
+
+							$sql = "INSERT INTO datos (id_datos,nombre,email) VALUES(NULL,'$nombreU','$emailU')";
+								$resultado = $mysqli->query($sql);
+							 ?>	
+				</div>
+				<footer>
+					<h3>Tecnicatura Sup. en Análisis y Programacion de Sistemas</h3>
+					<h4>Diseño de Interfaces-Capital 2017</h4>
+				</footer>
+
+			</div>
+
 </body>
 </html>
